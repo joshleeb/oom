6e55c5034acd452a941215928612e902aa1cda0c
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2018-03-29 16:27:08 +1100

    Setup basic sdl2 animation proof of concept

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,29 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::rect::Point;
+use sdl2::rect::{Point, Rect};
 use sdl2::render::Canvas;
+use sdl2::surface::Surface;
 use sdl2::video::Window;
+use std::path::Path;
+use std::thread;
+use std::time::Duration;
 
-const SCREEN_WIDTH: u32 = 800;
+static CHARACTER_SPRITE_PATH: &'static str = "assets/character.bmp";
+
+const FRAMES_PER_ANIMATION: i32 = 4;
 const SCREEN_HEIGHT: u32 = 600;
+const SCREEN_WIDTH: u32 = 800;
+const SPRITE_TILE_SIZE: u32 = 32;
+const SCALE: u32 = 4;
 
-fn empty_point() -> Point {
-    Point::new(0, 0)
+fn select_sprite(sprite_rect: &mut Rect, ticks: i32) {
+    sprite_rect.set_x(SPRITE_TILE_SIZE as i32 * ((ticks / 100) % FRAMES_PER_ANIMATION));
 }
 
-fn draw_points(canvas: &mut Canvas<Window>, points: &mut Vec<Point>) {
-    // Clear canvas or else we get some weird behaviour with a red background.
+fn clear_canvas(canvas: &mut Canvas<Window>) {
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
-
-    // Draw all the points. These draw commands get put into a back buffer and
-    // don't actually draw to the canvas until we call `present`.
-    for point in points {
-        canvas.set_draw_color(Color::RGB(255, 255, 255));
-        canvas.draw_line(empty_point(), point.clone()).unwrap();
-    }
-
-    // Actually draw to the canvas, everything in the back buffer.
-    canvas.present();
 }
 
 fn main() {
@@ -37,17 +35,32 @@ fn main() {
     let window = video_subsystem
         .window("game window", SCREEN_WIDTH, SCREEN_HEIGHT)
         .position_centered()
-        .opengl()
         .build()
         .unwrap();
 
-    let mut canvas = window.into_canvas().build().unwrap();
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-    canvas.present();
+    let mut canvas = window.into_canvas().accelerated().build().unwrap();
+    let texture_creator = canvas.texture_creator();
 
+    let character_surface = Surface::load_bmp(Path::new(CHARACTER_SPRITE_PATH)).unwrap();
+    let texture = texture_creator
+        .create_texture_from_surface(&character_surface)
+        .unwrap();
+
+    let mut timer = sdl_ctx.timer().unwrap();
     let mut events = sdl_ctx.event_pump().unwrap();
-    let mut points: Vec<Point> = vec![];
+
+    // Character walk animation sprite rect.
+    let mut sprite = Rect::new(0, 0, SPRITE_TILE_SIZE, SPRITE_TILE_SIZE);
+
+    // Rect on screen the sprite will be drawn into
+    let mut screen_rect = Rect::new(0, 0, SPRITE_TILE_SIZE * SCALE, SPRITE_TILE_SIZE * SCALE);
+    screen_rect.center_on(Point::new(
+        (SCREEN_WIDTH - SPRITE_TILE_SIZE) as i32 / 2,
+        (SCREEN_HEIGHT - SPRITE_TILE_SIZE) as i32 / 2,
+    ));
+
+    clear_canvas(&mut canvas);
+    canvas.present();
 
     'main: loop {
         for event in events.poll_iter() {
@@ -57,14 +70,17 @@ fn main() {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'main,
-                Event::MouseButtonDown { x, y, .. } => {
-                    println!("point {:?}", (x, y));
-
-                    points.push(Point::new(x, y));
-                    draw_points(&mut canvas, &mut points);
-                }
                 _ => {}
             }
         }
+
+        select_sprite(&mut sprite, timer.ticks() as i32);
+        clear_canvas(&mut canvas);
+        canvas
+            .copy(&texture, Some(sprite), Some(screen_rect))
+            .unwrap();
+        canvas.present();
+
+        thread::sleep(Duration::from_millis(100));
     }
 }
